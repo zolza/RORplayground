ea2cae5a21c85a0960a3534e801dc4f1b3260e08
zolza <kinga.koziol6@gmail.com>
2015-06-14 14:14:43 +0200

    hello world!

--- testapp/config/routes.rb
+++ testapp/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+
+
+  # You can have the root of your site routed with "root"
+  root 'application#hello'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
